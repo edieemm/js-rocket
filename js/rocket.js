a517c2a667d955f741dc4400d08e889035b587f7
Edith <edieb.543@gmail.com>
2019-02-28 10:26:30 +0300

    Made rocket cool with drawings

--- js/rocket.js
+++ js/rocket.js
@@ -6,11 +6,25 @@ var changeState = function (state) {
   clearInterval(timer);
   countdownNumber = 10;
   document.getElementById('countdown').innerHTML = countdownNumber;
+  document.getElementById('nervous').className = 'nervous';
+  document.getElementById('cant-wait').className = 'cant-wait';
 
   if (state == 2) {
     timer = setInterval(function () {
       countdownNumber = countdownNumber-1;
       document.getElementById('countdown').innerHTML = countdownNumber;
+      if (countdownNumber > 4 && countdownNumber <= 7) {
+        //be nervous
+        document.getElementById('nervous').className = 'nervous show';
+      } else{
+        document.getElementById('nervous').className = 'nervous';
+      }
+      if (countdownNumber > 1 && countdownNumber < 4) {
+        //be excited
+        document.getElementById('cant-wait').className = 'cant-wait show';
+      }else{
+        document.getElementById('cant-wait').className = 'cant-wait';
+      }
       if (countdownNumber <= 0) {
         changeState(3)
       };
@@ -19,7 +33,7 @@ var changeState = function (state) {
   var success = setTimeout(function () {
       var randomNumber = Math.round(Math.random()*10);
       console.log('randomNumber:',randomNumber)
-      if (randomNumber > 5) {
+      if (randomNumber > 2) {
           changeState(4);
       } else {
           changeState(5);
